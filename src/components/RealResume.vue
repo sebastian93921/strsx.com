fecc22ec5bdac27dee883a016fee74aa7c9882cf
sebastian93921 <sebastian93921@gmail.com>
2023-01-26 11:15:10 +0800

    Update RealResume.vue

--- src/components/RealResume.vue
+++ src/components/RealResume.vue
@@ -3,7 +3,7 @@
     <div class="header-container">
       <header class="wrapper clearfix">
         <h1 class="title">Sebastian Ko</h1>
-        <p>Senior Application Security Engineer | OSCE3 | OSCP </p>
+        <p>Senior Application Security Engineer | OSCE3 | OSEP | OSWE | OSED | OSCP </p>
       </header>
     </div>
     <main class="main-container">
