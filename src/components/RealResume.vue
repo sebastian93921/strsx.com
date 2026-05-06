74a1117da61c063b22414abca9a8e0acde091820
Sebastian Ko <sebastian93921@gmail.com>
2026-05-06 15:59:26 +0800

    Update RealResume.vue

--- src/components/RealResume.vue
+++ src/components/RealResume.vue
@@ -567,10 +567,10 @@ Print styles
   z-index: 10;
   width: 100px;
   height: 100px;
-  border-radius: 50%;
+  border-radius: 20%;
   overflow: hidden;
   canvas {
-    border-radius: 50%;
+    border-radius: 20%;
   }
 }
 
@@ -584,10 +584,10 @@ Print styles
     right: 15px;
     width: 100px;
     height: 100px;
-    border-radius: 50%;
+    border-radius: 20%;
     overflow: hidden;
     canvas {
-      border-radius: 50%;
+      border-radius: 20%;
     }
   }
 }
